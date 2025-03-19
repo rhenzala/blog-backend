15bc9a27f41900c2549d0a9d329f7e5b0169f00a
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-03-19 21:41:55 +0800

    Add middleware for cors

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
 const session = require("express-session");
 const routes = require("./routes/index");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true, 
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  }));
+  
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+
+  app.options("*", cors());
 
 app.use("/api/auth", routes.auth);
 app.use("/api/posts", routes.posts);
